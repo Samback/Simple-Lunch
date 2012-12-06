5862c5d618044c07364dbb6dc6cc99280361e125
Max Tymchii <samback@ukr.net>
2012-12-06 22:43:14 +0200

    Start to implement drief table of lunches

---
+++ LunchApp/Controllers/LABreafTableListOfLunchesController.h
@@ -0,0 +1,13 @@
+//
+//  LABreafTableListOfLunchesController.h
+//  LunchApp
+//
+//  Created by Max on 06.12.12.
+//  Copyright (c) 2012 Max Tymchii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LABreafTableListOfLunchesController : UITableViewController
+
+@end
