0c69e3365eb257d5854a17f8e31b93b82c569b84
Max Tymchii <samback@ukr.net>
2012-12-09 22:26:30 +0200

    Implement saving of data to core data.

--- LunchApp/Controllers/LAMyLunchController.m
+++ LunchApp/Controllers/LAMyLunchController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LAMyLunchController.h"
+#import "LunchPrevious.h"
 
 @interface LAMyLunchController ()
 @property (strong, nonatomic) IBOutlet UILabel *lunchName;
@@ -36,13 +37,23 @@
         descriptionFrame.origin.y = lunchNameFrame.origin.y + lunchNameFrame.size.height + 20;
     }
     else if (_selectedImages.count < 3){
-        descriptionFrame.origin.y = lunchNameFrame.origin.y + lunchNameFrame.size.height + 145;
+        descriptionFrame.origin.y = lunchNameFrame.origin.y + lunchNameFrame.size.height + 110;
+    }
+    else{
+         descriptionFrame.origin.y = lunchNameFrame.origin.y + lunchNameFrame.size.height + 210;
     }
     _LunchDescription.frame = descriptionFrame;
     for (int i = 0; i < _selectedImages.count; i++) {
-        UIImageView *img = (UIImageView *)_lunchImages[i];
-        img.backgroundColor = [UIColor redColor];
-        img.image = _selectedImages[i];
+//       // UIImageView *img = (UIImageView *)_lunchImages[i];
+//        UIImageView *img = [[UIImageView alloc] initWithImage:[LAHelpMethods imagefromAsset:(NSURL *)_selectedImages[i]]];
+//        img.backgroundColor = [UIColor redColor];
+//        ((UIImageView *)_lunchImages[i]).image =  img.image ;
+        UIImageView *currentIMV= (UIImageView*)_lunchImages[i];
+        CGRect imRect = currentIMV.frame;
+        imRect.origin.y = lunchNameFrame.origin.y + lunchNameFrame.size.height;
+        currentIMV.frame = imRect;
+        [LAHelpMethods imagefromAsset:_selectedImages[i] atImageView:currentIMV andRect:imRect];
+        
     }
         
 }
@@ -66,6 +77,16 @@
 - (void)addSelectedImages:(NSArray *)images{
     _selectedImages = images;
 }
+- (IBAction)saveToListOfLunches:(UIBarButtonItem *)sender {
+    [LunchPrevious newLunchWithName:_lunchName.text description:_LunchDescription.text andPhotos:_selectedImages];
+    NSError *error = nil;
+    [DELEGATE.managedObjectContext save:&error];
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:APP_NAME message:DONT_SAVED delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    }
+}
+
 - (void)viewDidUnload {
     [self setLunchName:nil];
     [self setLunchDescription:nil];
@@ -76,4 +97,5 @@
     [self setFourthImage:nil];
     [super viewDidUnload];
 }
+
 @end
